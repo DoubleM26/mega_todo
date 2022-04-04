3826ca3ca3a6ebfe9f3818b7d8013af79e33e2e7
banana-dan <79079738+banana-dan@users.noreply.github.com>
2022-04-04 11:53:53 +0300

    registration and authorisation added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, make_response, jsonify, render_template
+from werkzeug.utils import redirect
+
 from forms.login import LoginForm
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, login_required, logout_user
+from data import db_session
+from data.User import User
+from forms.registerform import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aboba'
@@ -14,20 +19,76 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+# презентационная страница
 @app.route('/')
 def main():
     return render_template("intro.html")
 
 
-@app.route('/register')
-def register():
-    return render_template("register.html")
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
-@app.route('/login')
+# авторизация
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/tasks")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
     return render_template("login.html", form=form, title="Авторизация")
+
+
+# страница с задачами
+@app.route('/tasks')
+def tasks():
+    return render_template("tasks.html")
+
+
+# настройки
+@app.route('/settings')
+def settings():
+    return render_template("settings.html")
+
+
+# выход из аккаунта
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.errorhandler(404)
@@ -36,7 +97,6 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    # db_session.global_init("db/super_todo.db")
+    db_session.global_init("db/super_todo.db")
     # app.register_blueprint(api.blueprint)
     app.run(port=8080, host='127.0.0.1')
-
