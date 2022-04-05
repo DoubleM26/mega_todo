1c2a2df90e8c14017f105343b1313ecde06be078
doublem26 <misha.261206@yandex.ru>
2022-04-05 10:30:54 +0300

    небольшие изменения

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, make_response, jsonify, render_template
 from werkzeug.utils import redirect
 
 from forms.login import LoginForm
-from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from data import db_session
 from data.User import User
 from forms.registerform import RegisterForm
@@ -19,20 +19,15 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-
-# презентационная страница
 @app.route('/')
 def main():
+    if current_user.is_authenticated:
+        return render_template("index.html", title="Mega ToDo")
     return render_template("intro.html")
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
@@ -55,7 +50,6 @@ def reqister():
     return render_template('register.html', title='Регистрация', form=form)
 
 
-# авторизация
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -64,26 +58,18 @@ def login():
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("/tasks")
+            return redirect("/")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template("login.html", form=form, title="Авторизация")
 
 
-# страница с задачами
-@app.route('/tasks')
-def tasks():
-    return render_template("tasks.html")
-
-
-# настройки
 @app.route('/settings')
 def settings():
-    return render_template("settings.html")
+    return render_template("settings.html", title="Настройки")
 
 
-# выход из аккаунта
 @app.route('/logout')
 @login_required
 def logout():
