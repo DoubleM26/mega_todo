3de2b7d68d5c227ee057b24a0cf6b36e561fd5a2
doublem26 <misha.261206@yandex.ru>
2022-04-18 18:06:15 +0300

    допочинка от Мишани

---
+++ static/js/index.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("task_create")
+form.addEventListener("keydown", (event) => {
+    if (event.code === "Enter") {
+        form.submit()
+    }
+})
+
+const task_search = document.getElementById("search_task")
+task_search.addEventListener('click', () => {
+    search.addEventListener("search", (event) => {
+        const href = window.location.href.split("/")
+            console.log(href)
+            if (search.value) {
+                if (href[3] === "complete_task") {
+                    window.location.replace(href.slice(0, 3).join("/") + "/" + "complete_tasks" + "/" + search.value)
+                } else if (href[3] === "task") {
+                    window.location.replace(href.slice(0, 3).join("/") + "/" + "todo" + "/" + search.value)
+                } else {
+                    window.location.replace(href.slice(0, 4).join("/") + "/" + search.value)
+                }
+            } else {
+                if (href[3] === "complete_task") {
+                    window.location.replace(href.slice(0, 3).join("/") + "/" + "complete_tasks")
+                } else if (href[3] === "task") {
+                    window.location.replace(href.slice(0, 3).join("/") + "/" + "todo")
+                } else {
+                    window.location.replace(href.slice(0, 4).join("/"))
+                }
+            }
+    })
+})
